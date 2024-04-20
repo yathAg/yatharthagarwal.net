6e8f80e59a4c4299f07b7b1b714d44a498300907
yathAg <yatharthrpi@gmail.com>
2024-04-20 17:00:13 -0400

    Buttons created, have to add filter functionality

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,23 +1,23 @@
 module.exports = {
   plugins: [
     {
-      resolve: "gatsby-theme-portfolio-minimal",
+      resolve: "Yatharth Agarwal",
       options: {
-        siteUrl: "https://gatsby-starter-portfolio-minimal-theme.netlify.app/", // Used for sitemap generation
+        siteUrl: "https://yatharthagarwal.netlify.app/", // Used for sitemap generation
         manifestSettings: {
           favicon: "./content/images/favicon.png", // Path is relative to the root
-          siteName: "My Minimal Portfolio", // Used in manifest.json
-          shortName: "Portfolio", // Used in manifest.json
+          siteName: "Yatharth Agarwal", // Used in manifest.json
+          shortName: "YA", // Used in manifest.json
           startUrl: "/", // Used in manifest.json
           backgroundColor: "#FFFFFF", // Used in manifest.json
           themeColor: "#000000", // Used in manifest.json
           display: "minimal-ui", // Used in manifest.json
         },
         contentDirectory: "./content",
-        blogSettings: {
-          path: "/blog", // Defines the slug for the blog listing page
-          usePathPrefixForArticles: false, // Default true (i.e. path will be /blog/first-article)
-        },
+        // blogSettings: {
+        //   path: "/blog", // Defines the slug for the blog listing page
+        //   usePathPrefixForArticles: false, // Default true (i.e. path will be /blog/first-article)
+        // },
         // googleAnalytics: {
         //     trackingId: "UA-XXXXXX-X",
         //     anonymize: true, // Default true

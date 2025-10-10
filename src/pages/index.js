b686ae75dd1925bd2a8ea037694a3f4051c167fc
Yatharth Agarwal <yatharthrpi@gmail.com>
2025-10-10 19:41:16 -0400

    revert?

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,12 @@ import {
   AboutSection,
   HeroSection,
   InterestsSection,
+  ArticlesSection,
   Page,
   Seo
 } from "gatsby-theme-portfolio-minimal";
 import ExperienceSection from "../gatsby-theme-portfolio-minimal/sections/Experiences/index.tsx";
 import ProjectSection from "../gatsby-theme-portfolio-minimal/sections/Projects/index.tsx";
-import ArticleSection from "../gatsby-theme-portfolio-minimal/sections/Articles_home/index.tsx";
 
 
 export default function IndexPage() {
@@ -21,7 +21,7 @@ export default function IndexPage() {
         <InterestsSection sectionId="details" heading="Interests"  buttonVisible="true" isPage="false"/>
         <ProjectSection sectionId="projects" heading="Projects" buttonVisible="true" isPage="false"/>
         <ExperienceSection sectionId="experience" heading="Experience" buttonVisible="true" isPage="false"/>
-        <ArticleSection sectionId="articles" heading="Recent Blogs" sources={['Blog']} />
+        <ArticlesSection sectionId="articles" heading="Recent Blogs" sources={['Blog']} />
       </Page>
     </>
   );
